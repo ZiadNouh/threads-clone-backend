04f9f78f646edbec6c4e50ae251d017b3658f551
ziad <ziad.nouh111@gmail.com>
2023-11-08 06:15:12 +0200

    a

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,6 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    const dbOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false, // optional
+      useCreateIndex: true, // optional
+    };
+
     await mongoose.connect(process.env.MONGO_URI).then(() => {
       console.log("mongoDB is connected");
     });
