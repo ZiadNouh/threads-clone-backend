9abf18e57432f20cf54d9bf8b2bc77fa20243412
ziad <ziad.nouh111@gmail.com>
2023-11-15 01:09:12 +0200

    f

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ dotenv.config();
 
 connectDB();
 const app = express();
+
+app.use(cookieParser());
 // app.use(cors(corsOptions));
 app.use(
   cors({
@@ -46,8 +48,6 @@ cloudinary.config({
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
