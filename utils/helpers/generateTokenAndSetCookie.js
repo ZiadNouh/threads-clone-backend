2e11d386b781deec914957c897d2d46403906a9a
ziad <ziad.nouh111@gmail.com>
2023-11-14 19:52:54 +0200

    editing token config

--- utils/helpers/generateTokenAndSetCookie.js
+++ utils/helpers/generateTokenAndSetCookie.js
@@ -8,7 +8,9 @@ const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true, // more secure
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    sameSite: "strict", // CSRF
+    // sameSite: "strict", // CSRF
+    sameSite: "None", // For cross-origin requests
+    secure: true, // Requires HTTPS
   });
 
   return token;
